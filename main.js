a1112f7a8b55c44be1c644b16bce5912a850b58c
DavidLucasOli <david.bengozi@escola.pr.gov.br>
2025-10-29 11:43:53 -0300

    f

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log(avanca)
 
 avanca.forEach(button =>{
     button.addEventListener('click', function(){
-        const atual =document.querySelector = 'passo' + getAttribute('data-proximo')
+        const atual =document.querySelector = 'passo' + this.getAttribute('data-proximo')
 
         atual.classList.remove('ativo')
         document.getElementById(proximoPasso).classList.add('ativo')
